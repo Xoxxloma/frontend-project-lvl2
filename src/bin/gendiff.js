3c74b999e387286516b5e62edd43ea1f2c5029cd
xoxxloma <xoxxloma@mail.ru>
2019-12-28 20:32:47 +0300

    first commit

---
+++ src/bin/gendiff.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+import program from 'commander';
+
+program
+  .version('1.0.0', '-V, --version', 'output the current version')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'Output format')
+  .parse(process.argv);

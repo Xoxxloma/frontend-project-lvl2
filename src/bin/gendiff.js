3e7a349ccf62c68c6d44e3b3e9766babb9f64992
xoxxloma <xoxxloma@mail.ru>
2019-12-29 12:29:31 +0300

    step 3

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,9 +1,14 @@
 #!/usr/bin/env node
 
 import program from 'commander';
+import gendiff from '..';
 
 program
   .version('1.0.0', '-V, --version', 'output the current version')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'Output format')
+  .arguments('<firstConfig> <secondConfig>')
+  .action((pathToFile1, pathToFile2) => {
+    console.log(gendiff(pathToFile1, pathToFile2));
+  })
   .parse(process.argv);
---
+++ src/index.js
@@ -0,0 +1,33 @@
+import { has, union } from 'lodash';
+import fs from 'fs';
+import path from 'path';
+import parse from './parsers';
+
+const gendiff = (pathToFile1, pathToFile2) => {
+  const data1 = fs.readFileSync(pathToFile1, 'utf-8');
+  const data2 = fs.readFileSync(pathToFile2, 'utf-8');
+
+  const type1 = path.extname(pathToFile1);
+  const type2 = path.extname(pathToFile2);
+
+  const obj1 = parse(data1, type1);
+  const obj2 = parse(data2, type2);
+
+  const keys = union(Object.keys(obj1), Object.keys(obj2)).sort();
+
+  const result = keys.map((key) => {
+    if (!has(obj2, key)) {
+      return (` - ${key}: ${obj1[key]}`);
+    }
+    if (!has(obj1, key)) {
+      return (` + ${key}: ${obj2[key]}`);
+    }
+    if (obj1[key] !== obj2[key]) {
+      return (` - ${key}: ${obj1[key]}\n + ${key}: ${obj2[key]}`);
+    }
+    return (`   ${key}: ${obj2[key]}`);
+  }).join('\n');
+  return `{\n${result}\n}`;
+};
+
+export default gendiff;

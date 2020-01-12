63196f9780a7023df4ab8d533efa735819c2dfbd
xoxxloma <xoxxloma@mail.ru>
2020-01-12 16:13:23 +0300

    refactoring extensions

--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ const gendiff = (pathToFile1, pathToFile2, format = 'tree') => {
   const data1 = fs.readFileSync(pathToFile1, 'utf-8');
   const data2 = fs.readFileSync(pathToFile2, 'utf-8');
 
-  const type1 = path.extname(pathToFile1);
-  const type2 = path.extname(pathToFile2);
+  const type1 = path.extname(pathToFile1).slice(1);
+  const type2 = path.extname(pathToFile2).slice(1);
 
   const obj1 = parse(data1, type1);
   const obj2 = parse(data2, type2);
--- src/parsers.js
+++ src/parsers.js
@@ -2,9 +2,9 @@ import yaml from 'js-yaml';
 import ini from 'ini';
 
 const parsers = {
-  '.json': JSON.parse,
-  '.yaml': yaml.safeLoad,
-  '.ini': ini.parse,
+  json: JSON.parse,
+  yaml: yaml.safeLoad,
+  ini: ini.parse,
 };
 
 const parse = (data, extension) => parsers[extension](data);

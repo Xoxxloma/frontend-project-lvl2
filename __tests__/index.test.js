36902afd8009e833ee8f9192ea03d4829ab9ab75
xoxxloma <xoxxloma@mail.ru>
2020-01-12 15:17:06 +0300

    remake tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,18 +2,17 @@ import fs from 'fs';
 import path from 'path';
 import gendiff from '../src';
 
-const types = ['json', 'yaml', 'ini'];
-const formats = ['tree', 'plain', 'json'];
 const getPath = (filename) => path.join(__dirname, `__fixtures__/${filename}`);
 
-const argues = formats.flatMap((format) => (
-  types.map((type) => [type, format])
-));
-
-test.each(argues)('%s', (type, format) => {
-  const before = getPath(`before.${type}`);
-  const after = getPath(`after.${type}`);
-  const actual = gendiff(before, after, format);
-  const expected = fs.readFileSync(getPath(format), 'utf-8');
-  expect(actual).toEqual(expected);
-});
+test.each([
+  ['json', 'tree'], ['yaml', 'tree'], ['ini', 'tree'],
+  ['json', 'plain'], ['yaml', 'plain'], ['ini', 'plain'],
+  ['json', 'json'], ['yaml', 'json'], ['ini', 'json']])(
+  '%s', (type, format) => {
+    const before = getPath(`before.${type}`);
+    const after = getPath(`after.${type}`);
+    const actual = gendiff(before, after, format);
+    const expected = fs.readFileSync(getPath(format), 'utf-8');
+    expect(actual).toEqual(expected);
+  },
+);
